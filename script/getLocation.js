b1aa04c8f215ea028225eceeec0079b2e91f8f98
TamajiNeko <classroom3.5.17@gmail.com>
2025-03-07 14:29:31 +0700

    xlsx test

--- script/getLocation.js
+++ script/getLocation.js
@@ -1,5 +1,5 @@
         // Target area settings
-        const radius = 2000; // 2 kilometers in meters
+        const radius = 500; // 2 kilometers in meters
         let targetLocations = [];
 
         // Fetch and process the Excel file when the page is loaded
